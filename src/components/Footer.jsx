48000f4ecf8827427d02642e79dfe4c6a06cca93
samuelolivermaia <samuelmaia1020@gmail.com>
2024-07-19 14:36:34 -0300

    Implemetação da Primeira Coluna

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,15 +7,25 @@ const Footer = () => {
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
         </p>
-        <div className="icnonis">
+        <div className="icones">
           <a href=""><i className="fab fa-facebook-f"></i></a>
           <a href=""><i className="fab fa-instagram"></i></a>
           <a href=""><i className="fab fa-twitter"></i></a>
         </div>
       </div>
-
+      <div className="footer-section info">
+        <h3>Infomação</h3>
+        <ul> 
+          <li><a href="#">Sobre Drip Store</a></li>
+          <li><a href="#">Segurança</a></li>
+          <li><a href="#">Wishlist</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Trabalhe Conosco</a></li>
+          <li><a href="#">Meus Pedidos</a></li>
+        </ul>
+      </div>
       <div>
-
+        
       </div>
     </footer>
     
