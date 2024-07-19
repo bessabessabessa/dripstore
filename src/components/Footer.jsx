0e3bab215cfe912926745172687c43def46c6e9c
samuelolivermaia <samuelmaia1020@gmail.com>
2024-07-19 13:58:22 -0300

    Implementação das Classe

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return(
+    <footer className="footera">
+      <div className="footer-section logo">
+        <h2>Digital Store</h2>
+        <img src="" alt="" />
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
+        </p>
+        <div className="icnonis">
+          <a href=""><i className="fab fa-facebook-f"></i></a>
+          <a href=""><i className="fab fa-instagram"></i></a>
+          <a href=""><i className="fab fa-twitter"></i></a>
+        </div>
+      </div>
+
+      <div>
+
+      </div>
+    </footer>
+    
+  );
+};
+
+export default Footer;
