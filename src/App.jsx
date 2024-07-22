752364b3475b73f975ad9aa043d55edb8702973d
unknown <RHAYSSA0483@digitalcollegesul.local>
2024-07-22 11:58:19 -0300

    atualizando header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Header from "./Components/Header/Header"
 import { Oferta } from './Components/Oferta/Oferta'
+import Footer from "./Components/Footer/Footer"
 // import { Nav } from 'react-bootstrap'
 
 function App() {
 
   return (
     <>
-      
+      <Footer/>
     </>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import './Footer.css'
+import LogoWhite from "../../assets/Images/logo-white.svg"
+import IconFace from "../../assets/Images/icon-face.svg"
+import IconInta from "../../assets/Images/icon-insta.svg"
+import IconTwitter from "../../assets/Images/icon-twitter.svg"
+
+const Footer = () => {
+
+  return (
+    <>
+      <div className="footer">
+        <div>
+          <div className="footerDigital">
+            <img src={LogoWhite} alt="" />
+            <p id="paragrafoDigital">Lorem ipsum dolor sit amet, consectetur<br />adipiscing elit, sed do eiusmod tempor<br />incididunt ut labore et dolore.</p>
+            <img id="icone1" src={IconFace} alt="" />
+            <img id="icone2" src={IconInta} alt="" />
+            <img id="icone3" src={IconTwitter} alt="" />
+
+          </div>
+          <div className="divInfos">
+            <h3>Informação</h3>
+            <p className="paragrafo1">Sobre Drip Store</p>
+            <p>Segurança</p>
+            <p>Wishlist</p>
+            <p>Blog</p>
+            <p>Trabalhe Conosco</p>
+            <p>Meus pedidos</p>
+          </div>
+          <div className="divCategorias">
+            <h3>Categorias</h3>
+            <p className="paragrafo1">Camistas</p>
+            <p>Calças</p>
+            <p>Bonés</p>
+            <p>Headphones</p>
+            <p>Tênis</p>
+          </div>
+          <div className="divContato">
+            <h3>Contato</h3>
+            <p className="paragrafo1">Av. Santos Dumont, 1510 - 1<br /> andar - Aldeota, Fortaleza-<br />CE, 60150-161</p>
+            <p>(85) 3051-3411</p>
+          </div>
+        </div>
+
+        <hr/>
+        <p id="textoAno">&copy; 2022 - Digital College</p>
+      </div>
+    </>
+
+  );
+};
+
+export default Footer;
