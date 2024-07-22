bea776db19e14a51d321cdfb35f04aa18c1bf8a9
rhayssa <rhayssa.costa@aluno.uece.br>
2024-07-22 14:42:18 -0300

    merge

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,19 @@
 import Carrossel from "../Components/Carrossel/Carrossel";
 import Destaque from "../Components/Destaque/Destaque";
+import Header from "../Components/Header/Header";
+import Oferta from "../Components/Oferta/Oferta";
+import Footer from "../Components/Footer/Footer";
+
 
 function HomePage() {
     return (
         <>
             <div>
+                <Header/>
                 <Carrossel/>
                 <Destaque/>
+                <Oferta/>
+                <Footer/>
             </div>
         </>
     )
