031cbbdc391065ce450a6d4c4ea63a7abb30c1ae
Alex Bessa <alexbessa.dev@gmail.com>
2024-07-22 11:37:45 -0300

    concluindo destaque

--- src/Components/Carrossel/Carrossel.jsx
+++ src/Components/Carrossel/Carrossel.jsx
@@ -2,32 +2,59 @@ import React from "react";
 import './Carrossel.css'
 import Ornament from "../../assets/Carrossel/Ornament.png"
 import Sneakers from "../../assets/Carrossel/Sneakers.png"
+import Fire from "../../assets/Carrossel/fire.png"
 
 const Carrossel = () => {
     return (
         <section class="carrossel">
-            <div class="divGeral">
-                <div class="div1">
+            <div class="divGeralCarrossel">
+                <div class="div1Carrossel">
                     <div class="chamada" >
                         <h3>Melhores ofertas personalizadas</h3>
-                        <h1>Queima de stoque Nike 🔥</h1>
+                        <h1>Queima de stoque Nike <img src={Fire} alt="" /></h1>
                         <p>Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.</p>
                         <button>Ver Ofertas</button>
                     </div>
                 </div>
-                <div className="div2">
+                <div className="div2Carrossel">
                     <div class="ornamento">
                         <img src={Ornament} alt="" />
                     </div>
                 </div>
-                <div className="div3">
+                <div className="div3Carrossel">
                     <div class="tenis">
                         <img src={Sneakers} alt="" />
                     </div>
+                    {/* <div id="carouselExampleIndicators" className="carousel slide">
+                        <div className="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src={Sneakers} className="d-block w-100" alt="..." />
+                            </div>
+                            <div className="carousel-item">
+                                <img src={Sneakers} className="d-block w-100" alt="..." />
+                            </div>
+                            <div className="carousel-item">
+                                <img src={Sneakers} className="d-block w-100" alt="..." />
+                            </div>
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div> */}
                 </div>
-                <div className="div4">
+                <div className="div4Carrossel">
                     <div class="slide">
-                        <input type="radio" name="redio" id="" checked/>
+                        <input type="radio" name="redio" id="" />
                         <input type="radio" name="redio" id="" />
                         <input type="radio" name="redio" id="" />
                         <input type="radio" name="redio" id="" />
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Carrossel from "../Components/Carrossel/Carrossel";
+import Destaque from "../Components/Destaque/Destaque";
 
 function HomePage() {
     return (
         <>
             <div>
                 <Carrossel/>
+                <Destaque/>
             </div>
         </>
     )
