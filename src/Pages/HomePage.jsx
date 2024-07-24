a95e7a481a46a264f2f9af583e59e70bee51af0f
samuel e sara <samuelmaia1020@gmail.com>
2024-07-24 09:46:21 -0300

    Inciando Page3

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,17 +3,14 @@ import Destaque from "../Components/Destaque/Destaque";
 import Header from "../Components/Header/Header";
 import Oferta from "../Components/Oferta/Oferta";
 import Footer from "../Components/Footer/Footer";
+import Detalhe from "../Components/Detalhe/Detalhe";
 
 
 function HomePage() {
     return (
         <>
             <div>
-                <Header/>
-                <Carrossel/>
-                <Destaque/>
-                <Oferta/>
-                <Footer/>
+                <Detalhe/>
 
                 
             </div>
